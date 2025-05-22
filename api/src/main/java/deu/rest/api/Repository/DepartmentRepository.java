e3ebf61aa92eb24147af3a85928d944b63d42cc7
DESKTOP-K2GGBTF\jimin <wlalswwkd0@naver.com>
2025-05-23 02:33:15 +0900

    단과대학 웹 크롤링 진행 중 등록금 코드 구현 필요  #2

--- api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
+++ api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
@@ -4,5 +4,7 @@ import deu.rest.api.Entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-    boolean existsByName(String name);
+    boolean existsByDepartmentName(String departmentName);
+    boolean existsByDepartmentPhone(String departmentPhone);
+
 }
--- api/src/main/java/deu/rest/api/Service/DepartmentService.java
+++ api/src/main/java/deu/rest/api/Service/DepartmentService.java
@@ -6,6 +6,7 @@ import deu.rest.api.Repository.DepartmentRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -20,11 +21,12 @@ import java.util.List;
 @Getter
 @Setter
 @AllArgsConstructor
+@Slf4j
 public class DepartmentService {
     private final DepartmentRepository departmentRepository;
     private final Department department = new Department();
 
-    public List<String> fetchMajorHomepages() throws IOException {
+    public List<String> fetchMajorHomepages() throws IOException { //하위 학과 링크 출력
         List<String> homepageLinks = new ArrayList<>();
 
         Document doc = Jsoup.connect("https://www.deu.ac.kr/www/deu-college.do").get();
@@ -38,5 +40,45 @@ public class DepartmentService {
         }
 
         return homepageLinks;
+    }
+
+    public void fetchAndSaveDepartment() throws IOException { //공통 메서드(학과 이름, 학과 전화번호) 호출
+        Document doc = Jsoup.connect("https://www.deu.ac.kr/www/deu-college.do").get();
+
+        Elements subjectElements = doc.select("div.subject");
+        Elements callElements = doc.select("div.call");
+
+        saveDepartmentNames(subjectElements);
+        saveDepartmentPhones(callElements);
+    }
+
+    private void saveDepartmentNames(Elements elements) {
+        for (Element element : elements) {
+            String name = element.text().trim();
+            log.info("단과대학 name: {}", name);
+
+            if (!departmentRepository.existsByDepartmentName(name)) {
+                Department department = new Department();
+                department.setDepartmentName(name);
+                departmentRepository.save(department);
+            }
+        }
+    }
+
+    private void saveDepartmentPhones(Elements elements) {
+        for (Element element : elements) {
+            String phone = element.text().trim();
+            log.info("단과대학 phone: {}", phone);
+
+            if (!departmentRepository.existsByDepartmentPhone(phone)) {
+                Department department = new Department();
+                department.setDepartmentPhone(phone);
+                departmentRepository.save(department);
+            }
+        }
+    }
+
+    private void fetchAndSaveTuition(){ //학과별 등록금 저장 로직 구현 필요
+        
     }
 }
