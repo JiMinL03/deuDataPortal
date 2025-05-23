04fe398d01d5ab7186b6e5db3d19ed1579aee91e
im-in-105 <146203551+im-in-105@users.noreply.github.com>
2025-05-23 13:40:51 +0900

    DepartmentName 오류 삭제

--- api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
+++ api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
@@ -4,5 +4,4 @@ import deu.rest.api.Entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-    boolean existsByName(String name);
 }
