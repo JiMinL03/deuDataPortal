bc7f8931afb833d810cd89ac78e740597f14cc58
DESKTOP-K2GGBTF\jimin <wlalswwkd0@naver.com>
2025-06-03 20:57:51 +0900

    professor 엔티티 오류 수정

--- api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
+++ api/src/main/java/deu/rest/api/Repository/DepartmentRepository.java
@@ -3,6 +3,7 @@ package deu.rest.api.Repository;
 import deu.rest.api.Entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
